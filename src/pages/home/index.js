c8e98572643b25bd32f1cd525236d991c071eb2f
Chetan Shewale <chetan.shewale@infobeans.com>
2023-12-24 14:37:55 +0530

    Home design

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
+
+import playstore from "../../assets/playstore.png";
 
 const Home = ({ navbarHeight }) => {
   const adjustedHeight = `calc(100% - ${navbarHeight}px)`;
@@ -7,27 +10,57 @@ const Home = ({ navbarHeight }) => {
   return (
     <div className="container-fluid h-100">
       <div className="row h-100">
-        {/* Left Column (Decreased from col-sm-3 to col-sm-2) */}
-        <div className="col-sm-2 d-none d-sm-block bg-info p-3">
-          {/* Content for the left column goes here */}
+        {/* Left Column */}
+        <div className="col-sm-1 d-none d-sm-block bg-success p-3">
           Left Column
         </div>
-
-        {/* Center Column (Increased from col-sm-6 to col-sm-8) */}
-        <div className="col-sm-8 bg-success d-flex align-items-center justify-content-center p-3" style={{height: adjustedHeight}}>
+        {/* Center Column */}
+        <div
+          className="col-sm-10 d-flex align-items-center justify-content-center p-3"
+          style={{ height: adjustedHeight }}
+        >
           {/* First Section */}
           <div className="text-center">
             <h2>Take the first step towards your profitable journey today!</h2>
             <p className="mt-3">
-              Welcome to AlgoTradeTech, We believe in accuracy and consistency
-              in trading and investments.
+              Welcome to AlgoTradeTech, We believe in Accuracy and Consistency
+              in every aspect of life and investments.
             </p>
+            <button type="button" className="btn btn-dark">
+              Learn More
+            </button>
+
+            {/* Updated additional content */}
+            <div className="d-flex justify-content-center mt-5">
+              <h6 className="col-sm-3 d-flex align-items-center justify-content-center no-bottom-margin">
+                Our App is live now!
+              </h6>
+              <div
+                className="col-sm-4 d-flex align-items-center justify-content-center"
+                style={{ color: "gray" }}
+              >
+                Download the app to get started
+              </div>
+              <div className="col-sm-3">
+                <img
+                  src={playstore}
+                  alt="Your Alt Text"
+                  style={{ width: "70%", height: "auto", maxWidth: "200px" }}
+                />
+              </div>
+            </div>
+
+            {/* Updated additional content */}
+            <div className="col-sm-12 mt-5 d-flex align-items-center justify-content-center no-bottom-margin">
+              Follow ATT on social media
+            </div>
+
+            {/* Gray divider */}
+            <div className="col-sm-12 mt-2 mb-3 mx-auto divider follow-us-divider align-self-center" />
           </div>
         </div>
-
-        {/* Right Column (Decreased from col-sm-3 to col-sm-2) */}
-        <div className="col-sm-2 d-none d-sm-block bg-warning p-3">
-          {/* Content for the right column goes here */}
+        {/* Right Column */}
+        <div className="col-sm-1 d-none d-sm-block bg-warning p-3">
           Right Column
         </div>
       </div>
